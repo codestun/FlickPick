41e87e8c5e3b4bbe06a68bfad9bd365eaecc995a
codestun <doerthy.k@gmail.com>
2023-09-05 18:09:20 +0200

    Restrict CORS to specific allowed origins.

--- index.js
+++ index.js
@@ -16,7 +16,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set up CORS
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://myflickpick.netlify.app'];
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // Import "auth.js" file
 let auth = require('./auth')(app);
